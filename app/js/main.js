a9d60da8b9d9b48190e33b7d4dca6ee873aacd85
jensfinnas <jens.finnas@gmail.com>
2014-10-15 17:17:03 -0700

    Add header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,31 @@ Hyphenator.config({
 	minwordlength : 8
 });
 Hyphenator.run();
+
+
+// Navbar: On start page, hide logo on top of scroll
+var $logo = $('.navbar-brand');
+var logoBreakPoint;
+var toggleLogo = function() {
+	if ($(window).scrollTop() >= logoBreakPoint) {
+		// Show logo
+		$logo
+		.addClass("fadeInLeft")
+		.removeClass('fadeOutLeft hidden')
+	} else {
+		// Hide logo
+		if ($logo.hasClass('fadeInLeft')) {
+			$logo.addClass("fadeOutLeft")
+				.removeClass("fadeInLeft hidden");			
+		}
+
+	}  
+
+};
+$(document).ready(function () {
+	// Only apply this effect on start page
+	if ($('body').hasClass('start')) {
+		logoBreakPoint = $('.jumbotron h1').offset().top;
+		document.onscroll = toggleLogo;
+	}
+})
